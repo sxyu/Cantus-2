f1cdbd0216eb23cccfad73137f413e71e1179b33
Alex Yu <alexyu99126@gmail.com>
2016-09-27 02:09:37 -0700

    Bumped version number to 2.5.1

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.5.0.134")>
-<Assembly: AssemblyFileVersion("2.5.0.134")>
+<Assembly: AssemblyVersion("2.5.1.136")>
+<Assembly: AssemblyFileVersion("2.5.1.136")>
